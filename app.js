097c98a76bc67430699615941eb66d472d57a2b8
Joseph Villajin <resetdesigns@gmail.com>
2022-04-08 14:22:37 -0700

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// const validator = require('validator');
-// console.log(validator.isURL('https://josephvillajin.com'));
-
-// const getNotes = require('./notes');
-// const msg = getNotes();
-// console.log(msg);
-
-//
-// Challenge: Use the chalk library in your project
-//
-// 1. Install version 2.4.1 of chalk
-// 2. Load chain into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-//
-// Bonus: Use the docs to mess around with other styles. Make text bold and inversed.
-//
-
-const chalk = require('chalk');
-console.log(chalk.green.bold.inverse('Success!'));
+const getNotes = require('./notes');
+const msg = getNotes();
+console.log(msg);
