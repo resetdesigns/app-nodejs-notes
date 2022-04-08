3dc127c8202be137afe7d0083b8913f4c74a759f
Joseph Villajin <resetdesigns@gmail.com>
2022-04-08 16:02:52 -0700

    reading and writing json with node.js

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
-const msg = getNotes();
-console.log(msg);
+
+// Customize yargs version
+yargs.version('1.1.0');
+
+// People should be able to add, remove, read, and list their notes
+// We want to show options and help text
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    },
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    },
+});
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all the notes');
+    },
+});
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a given note');
+    },
+});
+
+yargs.parse();
